06b44afe1743d5926ed4cc8eead9fdbc0e662fb7
Richard Kimber-Bell <rickykimberbell@gmail.com>
2019-04-20 18:14:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(arr,elem) {
   return [elem,...arr]
 }
+function destructivelyAddElementToBeginningOfArray(arr,elem) {
+  var newarr = [...arr]
+  newarr[0] = elem
+  return newarr
+}
