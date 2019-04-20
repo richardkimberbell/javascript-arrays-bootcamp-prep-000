916ede7ed3ba8144b2e428c0a0d4c313df782fbc
Richard Kimber-Bell <rickykimberbell@gmail.com>
2019-04-20 18:15:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,6 @@ function addElementToBeginningOfArray(arr,elem) {
   return [elem,...arr]
 }
 function destructivelyAddElementToBeginningOfArray(arr,elem) {
-  var newarr = [...arr]
-  newarr[0] = elem
-  return newarr
+   arr[0] = elem
+  return arr
 }
